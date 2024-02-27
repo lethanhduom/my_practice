ae3439dce2893ea83cef10e36fbcec37970b4a7a
lethanhduom <thanhduom9@gmail.com>
2024-02-28 00:26:37 +0700

    thêm practice

---
+++ practice_git/src/main/java/com/mycompany/practice_git/Practice_git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practice_git;
+
+/**
+ *
+ * @author hp_pc
+ */
+public class Practice_git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
