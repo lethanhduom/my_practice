44be35c680b578d6ab0ac1af901a893aa8a8faf8
lethanhduom <thanhduom9@gmail.com>
2024-02-28 13:11:55 +0700

    from userb

--- practice_git/src/main/java/com/mycompany/practice_git/branchb.java
+++ practice_git/src/main/java/com/mycompany/practice_git/branchb.java
@@ -1,17 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.practice_git;
 
 /**
  *
  * @author hp_pc
  */
-public class Practice_git {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("tôi là user b");
-    }
+public class branchb {
+    
 }
