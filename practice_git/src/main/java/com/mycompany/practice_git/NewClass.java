3267c8bb897a70cacaf092e8058b09797ca47d89
lethanhduom <thanhduom9@gmail.com>
2024-03-08 20:56:35 +0700

    update

--- practice_git/src/main/java/com/mycompany/practice_git/NewClass.java
+++ practice_git/src/main/java/com/mycompany/practice_git/NewClass.java
@@ -12,6 +12,7 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.print("thêm");
         System.out.println("from user a");
+        System.out.print("merge");
     }
     
 }
